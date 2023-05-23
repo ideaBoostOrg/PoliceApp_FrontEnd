544668e633ed0adac62620792beee93ed1b12591
Navod-Shehan <2019cs196@stu.ucsc.lk>
2023-05-23 08:11:02 +0530

    form created

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, { FC } from 'react';
-import { Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import ClearanceCertificateApplication from './components/ClearanceCertificateApplication';
 
 
 const App: FC = () => {
   return (
     <div>
-      <Home/>
-    <Router location={''} navigator={undefined}>
+    <Router>
       
         <Routes>
           <Route  path="/" Component={Home} />
           <Route  path="/about" Component={About} />
+          <Route path="/certificateApplication" Component={ClearanceCertificateApplication} />
 
         </Routes>
       
