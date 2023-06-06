3f847ac35231a85265b3205bb0977e863aeb12ae
VenushkaMalinda <pvvmchandrarathna@gmail.com>
2023-06-06 16:21:55 +0530

    Responsive Views

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -7,8 +7,8 @@ const Dashboard: FC = () => {
   return (
     <>
       <div className="dashboard-header">
-        <h2>Welcome to the Police Clearance Certificate Issuance e-Service Online Application</h2>
-        <p className='text'>Online Clearance Issuance System is another remarkable step taken by Sri Lanka Police to enhance people friendly professional service</p>
+        <h2 className='home-header'>Welcome to the Police Clearance Certificate Issuance e-Service Online Application</h2>
+        <p className="instruction-text">Online Clearance Issuance System is another remarkable step taken by Sri Lanka Police to enhance people friendly professional service</p>
       </div>
 
       <Card title={<h1 className='header'>Our Services</h1>} bordered={false} className="dashboard-card">
